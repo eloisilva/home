104f57217e001568b84047e0c5467742148f9df6
Eloi Silva <etch.linux@gmail.com>
2018-07-16 16:08:38 -0300

    Update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-PACKAGES="/root/packages.txt"
+PACKAGES="/root/home/packages.txt"
+GITHOME="https://github.com/eloisilva/home.git"
+INSTALL_DIR="$HOME/home/install/"
 
 check(){
    [ -f $PACKAGES ] && echo "[OK] Package file $PACKAGES found" || ERROR="[Error] Package file not found" error
@@ -11,14 +13,26 @@ error(){
    exit 1
 }
 
-install(){
+install_packages(){
    ALL=`grep -v ^"#" $PACKAGES |grep . |xargs -n1000`
    apt install -y $ALL
 }
 
+install_dir(){
+   for file in `ls -1 $INSTALL_DIR` ;do
+      file=${INSTALL_DIR}${file}
+      if [ -x ${file} ] ;then
+         log=${file}.log
+         echo "[Installing] `basename $file`..."
+         $file > $log 2>&1 && echo "[OK] `basename $file`" || echo "[Error] `basename $file`"
+      fi
+   done
+}
+
 main(){
    check
-   install
+   install_packages
+   install_dir
 }
 
 main
