50edf68e72be055ccae90c11dd603bf541b16e56
Eloi Silva <etch.linux@gmail.com>
2020-08-30 13:25:08 +0100

    Added bash_aliases 72_wiki.sh: Create SSH tunnel to wiki instance and open wiki on default browser (MacOS Firefox binary)

---
+++ .bash_aliases.d/72_wiki.sh
@@ -0,0 +1,32 @@
+#################################################################################
+#     File Name           :     tmux.sh
+#     Created By          :     Eloi Silva
+#     Creation Date       :     [2020-08-30 13:11]
+#     Description         :     Bash TMUX aliases & functions
+#################################################################################
+
+# Default browser binary
+BROWSER="/Applications/Firefox.app/Contents/MacOS/firefox --new-tab"
+
+
+# Create SSH tunnel and open wiki
+wiki(){
+   WIKI="admin@2.2.2.2"
+   IP="${WIKI:-$1}"
+   if ! lsof -nPiTCP:8080 -sTCP:LISTEN > /dev/null 2>&1 ;then
+      ssh -fNL 8080:localhost:8000 $WIKI
+   fi
+   $BROWSER http://localhost:8080
+   unset IP
+}
+
+# Create SSH tunnel and open dokuwiki
+dwiki(){
+   DWIKI="admin@2.2.2.2"
+   IP="${DWIKI:-$1}"
+   if ! lsof -nPiTCP:8443 -sTCP:LISTEN >/dev/null 2>&1 ;then
+      ssh -fNL 8443:localhost:443 $DWIKI
+   fi
+   $BROWSER https://localhost:8443
+   unset IP
+}
