54c371fd580bd50d50364e5635c3aaca854a2d14
Eloi Silva <etch.linux@gmail.com>
2022-12-27 22:27:29 -0300

    Update SSH config and check.py

--- check.py
+++ check.py
@@ -3,7 +3,7 @@
 #     File Name           :     check.py
 #     Created By          :     Eloi Silva
 #     Creation Date       :     [2020-06-09 01:15]
-#     Last Modified       :     [2022-07-25 12:50]
+#     Last Modified       :     [2022-12-27 13:35]
 #     Description         :     Check if files was updated on home directory
 ###############################################################################
 
@@ -13,7 +13,7 @@ import hashlib
 
 user_home = os.getenv('HOME')
 files = dict(
-    vim=['./.vimrc', './.vim/plugins.vim', './.vim/after/ftplugin/python.vim', './.vim/after/ftplugin/c.vim', './.vim/after/ftplugin/sh.vim', './.vim/after/ftplugin/sh.vim'],
+    vim=['./.vimrc', './.vim/plugins.vim', './.vim/after/ftplugin/python.vim', './.vim/after/ftplugin/c.vim', './.vim/after/ftplugin/sh.vim', './.vim/after/ftplugin/markdown.vim', './.vim/after/ftplugin/yaml.vim', './.vim/after/ftplugin/html.vim'],
     bash=['./.bashrc', './.inputrc'],
     bash_aliases=['./.bash_aliases.d/01_default.sh', './.bash_aliases.d/50_tmux.sh', './.bash_aliases.d/70_aws.sh', './.bash_aliases.d/72_wiki.sh', './.bash_aliases.d/71_workdir.sh', './.bash_aliases.d/80_ssh.sh', './.bash_aliases.d/81_git.sh'],
     tmux=['./.tmux.conf', './.tmux/tmux-color.conf'],
